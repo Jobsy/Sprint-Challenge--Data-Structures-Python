b11daaf3a61a2141c94f066412984cd451f1bdf7
Oluwajoba <oluwajobabello@gmail.com>
2020-02-28 10:48:40 +0100

    second pass / using 'set' / stretch / names.py

--- names/names.py
+++ names/names.py
@@ -20,16 +20,31 @@ duplicates = []  # Return the list of duplicates in this data structure
 
 
 # first-pass using a single 'for-loop'
-for name in names_1:
-    if name in names_2:
-        duplicates.append(name)
+# for name in names_1:
+#     if name in names_2:
+#         duplicates.append(name)
 
-
-end_time = time.time()
-print(f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
-print(f"runtime: {end_time - start_time} seconds")
+# end_time = time.time()
+# print(f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
+# print(f"runtime: {end_time - start_time} seconds")
 
 # ---------- Stretch Goal -----------
 # Python has built-in tools that allow for a very efficient approach to this problem
 # What's the best time you can accomplish?  Thare are no restrictions on techniques or data
 # structures, but you may not import any additional libraries that you did not write yourself.
+
+# second-pass using a single 'set'
+def duplicates_func(input_1, input_2):
+
+    temp = set(input_2)
+    global duplicates
+    duplicates = [value for value in input_1 if value in temp]
+
+    return duplicates
+
+
+duplicates_func(names_1, names_2)
+
+end_time = time.time()
+print(f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
+print(f"runtime: {end_time - start_time} seconds")
