eb33b5f92143f34fff766d1cda6fa42e3ad44ce0
Oluwajoba <oluwajobabello@gmail.com>
2020-02-28 13:41:20 +0100

    complete 'append' and 'get' method / ring_buffer.py

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -8,13 +8,44 @@ class RingBuffer:
         self.storage = DoublyLinkedList()
 
     def append(self, item):
-        pass
+        # if key in self.storage:
+        if self.storage.length >= 0 and self.storage.length < self.capacity:
+            # node = self.storage[key]
+            # node.value = (key, value)
+            # self.storage.move_to_end(item)
+            self.storage.add_to_tail(item)
+            self.current = self.storage.tail
+            # return
+
+        # if self.current_num_of_node == self.limit:
+        # elif self.storage.length == self.capacity:
+        elif self.current == self.storage.tail:
+            # del self.storage[self.key_value_entries.head.value[0]]
+            # self.key_value_entries.remove_from_head()
+            self.storage.remove_from_head()
+            self.storage.add_to_head(item)
+            self.current = self.storage.head
+
+            # self.current_num_of_node -= 1
+        else:
+            # self.key_value_entries.add_to_tail((key, value))
+            self.current.insert_after(item)
+            # self.storage[key] = self.key_value_entries.tail
+            # self.current_num_of_node += 1
+            self.storage.length += 1
+            self.current = self.current.next
+            self.storage.delete(self.current.next)
 
     def get(self):
         # Note:  This is the only [] allowed
         list_buffer_contents = []
 
         # TODO: Your code here
+        current = self.storage.head
+
+        while current != None:
+            list_buffer_contents.append(current.value)
+            current = current.next
 
         return list_buffer_contents
 
